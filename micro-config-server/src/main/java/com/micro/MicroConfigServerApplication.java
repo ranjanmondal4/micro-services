2671e517283ab5945adca8016e1c5c0b00316a14
ranjan mondal <ranjan.mondal@oodlestechnologies.com>
2019-04-20 00:12:49 +0530

    server one not fetching config server

--- micro-config-server/src/main/java/com/micro/MicroConfigServerApplication.java
+++ micro-config-server/src/main/java/com/micro/MicroConfigServerApplication.java
@@ -3,15 +3,18 @@ package com.micro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
+//@EnableEurekaClient
 public class MicroConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroConfigServerApplication.class, args);
 	}
-
 }
